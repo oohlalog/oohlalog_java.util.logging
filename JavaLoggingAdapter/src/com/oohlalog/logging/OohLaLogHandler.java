2c09df60b9efa5d42da7a28190a3cf80f8daf5b0
JulianGriggs <jgriggs@princeton.edu>
2014-06-19 18:37:24 -0700

    Removed unused imports

--- JavaLoggingAdapter/src/com/oohlalog/logging/OohLaLogHandler.java
+++ JavaLoggingAdapter/src/com/oohlalog/logging/OohLaLogHandler.java
@@ -2,9 +2,7 @@ package com.oohlalog.logging;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
